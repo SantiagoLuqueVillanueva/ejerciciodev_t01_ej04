f0033a51ff3eb8dd24bcb812798b95cf6c5bfbeb
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-09-24 22:12:10 +0200

    Horario de clase hecho

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Lunes  Martes  Miércoles  Jueves  Viernes");
+        System.out.println("=====  ======  =========  ======  =======");
+        System.out.printf("%-6s %-7s %-10s %-7s %s","SIINF", "PROGR", "SIINF", "IPE 1", "BADAT\n");
+        System.out.printf("%-6s %-7s %-10s %-7s %s","SIINF", "PROGR", "SIINF", "IPE 1", "BADAT\n");
+        System.out.printf("%-6s %-7s %-10s %-7s %s","LMSGI", "PROGR", "IPE 1", "PROGR", "SIINF\n");
+        System.out.printf("%-6s %-7s %-10s %-7s %s","BADAT", "BADAT", "PROGR", "PROGR", "ENDES\n");
+        System.out.printf("%-6s %-7s %-10s %-7s %s","BADAT", "SASP", "PROGR", "LMSGI", "ENDES\n");
+    
     }
 }
